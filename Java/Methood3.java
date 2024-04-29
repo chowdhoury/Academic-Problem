636a52ad04800953adce4b7c1f1fb25c8c255b4e
Md Raihan Chowdhoury <mdr8077@gmail.com>
2024-04-30 01:18:02 +0600

    Create Methood3.java

---
+++ Java/Methood3.java
@@ -0,0 +1,10 @@
+public class Methood3{
+    public static void main(String args[]){
+        int a=20,b=30;
+        Methood3 obj= new Methood3();
+        obj.sum(a,b);
+    }
+    public void sum(int x,int y){
+        System.out.println(x+y);
+    }
+}

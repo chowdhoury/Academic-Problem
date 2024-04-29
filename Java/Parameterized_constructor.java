447dacfbdf08b4582376c69265c9f1b665a46ce5
Md Raihan Chowdhoury <mdr8077@gmail.com>
2024-04-30 04:18:35 +0600

    Create Parameterized_constructor.java

---
+++ Java/Parameterized_constructor.java
@@ -0,0 +1,10 @@
+public class Myclass{
+    int x;
+    public Myclass(int a){
+        x=a;
+    }
+    public static void main(String args[]){
+        Myclass obj=new Myclass(50);
+        System.out.println(obj.x);
+    }
+}

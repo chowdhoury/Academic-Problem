a3772dfd43621c5cd2a1f1389b5fc07c0433395e
Md Raihan Chowdhoury <mdr8077@gmail.com>
2024-04-30 03:58:13 +0600

    Create Default_Constructor.java

---
+++ Java/Default_Constructor.java
@@ -0,0 +1,10 @@
+public class Myclass{
+    int x;
+    public Myclass(){
+        x=5;
+    }
+    public static void main(String args[]){
+        Myclass obj=new Myclass();
+        System.out.println(obj.x);
+    }
+}

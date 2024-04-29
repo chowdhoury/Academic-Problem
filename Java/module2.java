5180ca9ee93f4fb123405233761d76bf2312a6ed
Md Raihan Chowdhoury <mdr8077@gmail.com>
2024-04-30 01:06:34 +0600

    Create module2.java

---
+++ Java/module2.java
@@ -0,0 +1,9 @@
+public class Methood1{
+    public static void main(String args[]){
+        int a=10,b=5;
+        max(a,b);
+    }
+    public static void max(int x,int y){
+        System.out.println((x>y)? x : y);
+    }
+}

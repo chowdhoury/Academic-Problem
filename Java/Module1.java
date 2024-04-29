7583ab8aedd64a47021fe7a0a60f31d04f7122f8
Md Raihan Chowdhoury <mdr8077@gmail.com>
2024-04-30 01:03:27 +0600

    Java uplaod

---
+++ Java/Module1.java
@@ -0,0 +1,9 @@
+public class Methood1{
+    public static void main(String args[]){
+        int a=10,b=5;
+        sum(a,b);
+    }
+    public static void sum(int x,int y){
+        System.out.println(x+y);
+    }
+}

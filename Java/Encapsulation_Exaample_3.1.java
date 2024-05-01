283c8e83a1cf2bc90e8ba95363c56a0a0640bf72
Md Raihan Chowdhoury <mdr8077@gmail.com>
2024-05-01 21:51:39 +0600

    Create Encapsulation_Exaample_3.1.java

---
+++ Java/Encapsulation_Exaample_3.1.java
@@ -0,0 +1,37 @@
+package com.raihan.myclass;
+public class MyClass {
+
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.setName("Raihan");
+        s1.setId(1002);
+        s1.setCgpa(3.77);
+        System.out.println("Name: "+s1.getName());
+        System.out.println("Id: "+s1.getId());
+        System.out.println("Cgpa: "+s1.getCgpa());
+    }
+}
+package com.raihan.myclass;
+public class Student {
+    private String name;
+    private int id;
+    private double cgpa;
+    public void setName(String a){
+        this.name=a;
+    }
+    public void setId(int b){
+        this.id=b;
+    }
+    public void setCgpa(double c){
+        this.cgpa=c;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+    public double getCgpa(){
+        return cgpa;
+    }
+}
